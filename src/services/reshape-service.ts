7427090730e72bab509cde5e41b505a94a1a125a
root <root@srv-api-oci.localdomain>
2024-04-02 10:16:40 -0500

    feat: nova VM atualizacao

--- src/services/reshape-service.ts
+++ src/services/reshape-service.ts
@@ -2,5 +2,6 @@ import { getReshapeRepository } from "@/repositories";
 
 export async function getReshapeService() {
     const response = await getReshapeRepository();
+    console.log(response?.length);
     return response;
 }
