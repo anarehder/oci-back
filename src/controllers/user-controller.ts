33b381d10eda234d70b6fd8de84b867dd0c73ad7
anarehder <anapaula.rehder@gmail.com>
2024-05-13 10:37:13 -0300

    feat: criar usuario

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -7,7 +7,7 @@ export async function createUserController (req: Request, res: Response) {
     const { username, password, isAdmin, client } = req.body as NewUserInput;
     try{
         await createUserService ( username, password, isAdmin, client );
-        return res.status(httpStatus.OK).send(`User ${username} was created !`);
+        return res.status(httpStatus.CREATED).send(`User ${username} was created !`);
     } catch (error) {
         return res.status(httpStatus.UNAUTHORIZED).send(error);
     }
